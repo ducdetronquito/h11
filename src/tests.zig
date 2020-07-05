3f6239284dad833dbf17572e5f18587047363840
ducdetronquito <guillaume.paulet@giome.fr>
2020-07-05 16:33:47 +0200

    :sparkles: Headers parser.

--- src/tests.zig
+++ src/tests.zig
@@ -1,6 +1,1 @@
-usingnamespace @import("buffer.zig");
-usingnamespace @import("client.zig");
-usingnamespace @import("connection.zig");
-usingnamespace @import("events.zig");
-usingnamespace @import("states.zig");
-usingnamespace @import("stream.zig");
+usingnamespace @import("headers.zig");
